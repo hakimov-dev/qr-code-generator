d87309913aaabd6a933efad0c82cdbff3ed70999
Muhammadamin <webhacker0100@gmail.com>
2022-11-27 17:41:18 +0500

    Created new variable

--- store/index.js
+++ store/index.js
@@ -1,7 +1,9 @@
 import { createStore } from "vuex";
 
 export default createStore({
-    state: {},
+    state: {
+        newQrCodeUrl: null
+    },
     getters: {},
     mutations: {},
     actions: {},
