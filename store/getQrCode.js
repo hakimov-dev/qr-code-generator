63d817e182cda5bb1595a9112fcb94d72f5b69d8
Muhammadamin <webhacker0100@gmail.com>
2022-11-27 16:41:48 +0500

    Created new variable

--- store/getQrCode.js
+++ store/getQrCode.js
@@ -4,8 +4,9 @@ import { defineStore } from 'pinia'
 export const createQrCode = defineStore({
     id: 'qr-code',
     state: () =>  ({
+      newQrCodeLink: null,
     }), 
     actions: {
-        
+
     }
 })
