d33a50205b3ce3dd1082c09bafa0926443aee9da
Muhammadamin <webhacker0100@gmail.com>
2022-11-27 16:36:58 +0500

    Added card

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <HeaderNavbar/>
+  <div>
+    <HeaderNavbar/>
+    <a-card hoverable style="width: 400px; margin:0 auto; margin-top: 5%;">
+    <template #cover>
+      <img
+        alt="example"
+        class="w-[400px] h-[300px] pointer-events-none select-none"
+        src="../assets/imgs/default.png"
+      />
+      <br>
+      <div class="flex w-[90%] text-center items-center justify-center flex-col">
+        <a-input class="ml-[10%]" v-model:value="text" size="large" placeholder="Enter text for convert to qr-code" title="Qr-code"/>
+        <button class="px-[6px] py-[8px] rounded bg-blue-400 text-[16px] font-semibold text-white mt-4 ml-[10%] hover:-translate-y-2 hover:bg-black">Generate QR-CODE</button>
+      </div>
+      <br>
+    </template>
+    <template #actions>
+       <p class="text-[16px] text-blue-400">SVG</p>
+       <p class="text-[16px] text-blue-400">PNG</p>
+       <p class="text-[16px] text-blue-400">JPG</p>
+    </template>
+  </a-card>
+  </div>
 </template>
 
 <script>
