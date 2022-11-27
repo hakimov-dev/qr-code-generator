4c76776e19b54926d0dbb463a869160c8079f661
Muhammadamin <webhacker0100@gmail.com>
2022-11-27 14:35:26 +0500

    Update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'qr-code-generator',
+    title: 'QR code generator',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
