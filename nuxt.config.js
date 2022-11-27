a62964a055210792efe37a8d497c04630fc3ad63
Muhammadamin <webhacker0100@gmail.com>
2022-11-27 14:48:57 +0500

    Update file path

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     'ant-design-vue/dist/antd.css',
-    '@/assets/css/tailwind.css'
+    '@/assets/css/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
